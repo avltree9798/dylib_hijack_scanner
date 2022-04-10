64b7922a55d316a6fb7657771babb27aea079c34
Anthony Viriya <anthony.viriya@gopay.co.id>
2022-04-10 02:06:37 +0100

    Add a unit test

--- tests/dylib_hijack_scanner_test.py
+++ tests/dylib_hijack_scanner_test.py
@@ -1,8 +1,21 @@
 import unittest
+import os
+from unittest.mock import patch
+from modules.dylib_hijack_scanner import DylibHijackScanner
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+OS_WALK_RETURN = [
+	('/foo', ('bar',), ('baz',)),
+	('/foo/bar', (), ('spam', 'eggs')),
+]
+
+
+class DylibHijackScannerTest(unittest.TestCase):
+	@patch.object(os, 'walk')
+	def test_get_all_files(self, walk):
+		walk.return_value = OS_WALK_RETURN
+		scanner = DylibHijackScanner(directory_to_scan='/Applications/', output='')
+		self.assertEqual(scanner.files_to_scan, ['/foo/baz', '/foo/bar/spam', '/foo/bar/eggs'])
+
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
