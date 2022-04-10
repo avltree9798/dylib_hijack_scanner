1b5e4e8581c1527fd7979f453c46b0a7d7aed992
Anthony Viriya <anthony.viriya@gopay.co.id>
2022-04-10 21:43:41 +0100

    Fixing test case

--- tests/dylib_hijack_scanner_test.py
+++ tests/dylib_hijack_scanner_test.py
@@ -9,7 +9,8 @@ class DylibHijackScannerTest(unittest.TestCase):
 			'tests/fake_storage/Applications/TestApp2.app/Content/MacOS/TestApp2',
 			'tests/fake_storage/Applications/TestApp1.app/Content/MacOS/TestApp1'
 		]
-
+		scanner.files_to_scan.sort()
+		expected_returns.sort()
 		self.assertEqual(scanner.files_to_scan, expected_returns)
 
 	def test_perform_rpath_scanning(self):
